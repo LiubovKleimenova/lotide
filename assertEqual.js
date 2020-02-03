e9fc2fba726ef7da04978aa3bcce1b514a5e848e
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-02-03 20:20:34 +0000

    created assert function

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log("🔥🔥🔥Assertion failed: " + actual + " !== " + expected);
+  } else {
+    console.log("🍏🍏🍏Assertion passed: " + actual + " !== " + expected);
+  }
+};
+//  TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
