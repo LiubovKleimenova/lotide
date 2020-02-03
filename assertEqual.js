3a7f849e8cf4a0b0a675e71ea6eb4a9263a47c4f
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-02-03 20:27:43 +0000

    used literals instead of string concat

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,13 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
-    console.log("🔥🔥🔥Assertion failed: " + actual + " !== " + expected);
+    console.log(`🔥🔥🔥Assertion failed:  ${actual}  !== ${expected}`);
   } else {
-    console.log("🍏🍏🍏Assertion passed: " + actual + " !== " + expected);
+    console.log(`🍏🍏🍏Assertion passed: ${actual}  == ${expected}`);
   }
 };
 //  TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
 assertEqual(1, 1);
+assertEqual("1", 1);
