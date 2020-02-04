77de94b0db8d7a55e825cf430ab642d6e49ac75d
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-02-04 22:53:28 +0000

    function created

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🔥🔥🔥Assertion failed:  ${actual}  !== ${expected}`);
+  } else {
+    console.log(`🍏🍏🍏Assertion passed: ${actual}  == ${expected}`);
+  }
+};
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); 
+console.log(assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), true)); 
+console.log(assertEqual(eqArrays([1, 2, "3"], [1, 2, 3]), true)); 
+console.log(assertEqual(eqArrays([1, 2], [1, 2, 3]), true)); 
