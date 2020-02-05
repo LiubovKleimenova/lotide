bcf1fd975fa46d77764f5f873899eec945d5f597
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-02-05 21:33:24 +0000

    added fn to compare obj

--- eqObjects.js
+++ eqObjects.js
@@ -1,19 +1,44 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const eqObjects = function(obj1, obj2) {
   if (Object.keys(obj1).length !== Object.keys(obj2).length) {
     return false;
   } 
   for (let key of Object.keys(obj1)) {
-    if (obj1[key] !== obj2[key]) {
-      return false;
+    if (!Array.isArray(obj1[key])) {
+      if (obj1[key] !== obj2[key]) {
+        return false;
+      }
+    } else {
+      return eqArrays(obj1[key], obj2[key]);
+      
     }
+    
   }
   return true;
 }
 
 const abc = { a: "1", b: "2", c: "3" };
-const ab = { a: "1", b: "2", c: "3" };
-const ba = { c: "3", b: "2", a: "1" };
-//eqObjects(ab, abc); // => false
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+console.log(eqObjects(ab, abc)); // => false
 
 
 console.log(eqObjects(ab, ba)); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log(eqObjects(cd, cd2));
+console.log(eqObjects(cd, dc));
