7cb40bc99b2318326ef2b3c6aff1d030587a7f8f
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-02-05 20:40:15 +0000

    added findKeyByVal fn

---
+++ findKeyByValue.js
@@ -0,0 +1,19 @@
+let findKeyByValue = function(obj, val) {
+  let keysArr = Object.keys(obj);
+  console.log(keysArr);
+  for (let key of keysArr) {
+    if (val === obj[key]) {
+      return key;
+    }
+  }
+}
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+  onemoregenre: "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The "));
+//assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
