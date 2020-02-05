1f7d10afbb1d3f1309b87bdc9c75e94a367cd1ae
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-02-05 20:17:36 +0000

    added letterPos fn

---
+++ letterPositions.js
@@ -0,0 +1,18 @@
+const letterPositions = function(str) {
+  let newObj = {};
+  newStr = str.split("");
+  
+
+  for (let [index, letter] of newStr.entries()) {
+    if (letter !== " ") {
+      if (!newObj[letter]) {
+        newObj[letter] = [];
+      }
+      newObj[letter].push(index);
+    }
+  }
+
+  return newObj;
+}
+
+console.log(letterPositions("lighthouse in the house"));
