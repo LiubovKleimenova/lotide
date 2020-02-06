c30be6a49fea969c1a312562d5cba75ea418179c
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-02-06 14:12:58 -0500

    added takeUntil fn

---
+++ takeUntil.js
@@ -0,0 +1,31 @@
+const takeUntil = function(arr, cb) {
+  let resArr = []
+  let i = 0;
+  while (!cb(arr[i])) {
+    resArr.push(arr[i]);
+    i++;
+  }
+
+  return resArr;
+};
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log("---");
+
+const data2 = [
+  "I've",
+  "been",
+  "to",
+  "Hollywood",
+  ",",
+  "I've",
+  "been",
+  "to",
+  "Redwood"
+];
+const results2 = takeUntil(data2, x => x === ",");
+console.log(results2);
