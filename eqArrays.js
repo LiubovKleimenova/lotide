f0d0a8b0d17c60d57927bb915396f2e8c73536a6
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-02-07 16:11:36 -0500

    modified eqArrays fn

--- eqArrays.js
+++ eqArrays.js
@@ -7,18 +7,23 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function (arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
-  
+
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+    if (Array.isArray(arr1[i])) {
+      console.log("more arrays inside");
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+    } else if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
   return true;
-}
+};
 
 
 console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); 
--- eqObjects.js
+++ eqObjects.js
@@ -4,7 +4,12 @@ const eqArrays = function(arr1, arr2) {
   }
 
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+    if (Array.isArray(arr1[i])) {
+      console.log("more arrays inside");
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+    } else if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
