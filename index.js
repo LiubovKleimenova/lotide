eb2661b157c107781385d77e50ea48f629c471ab
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-02-10 13:43:30 -0500

    shortened fns

--- index.js
+++ index.js
@@ -17,21 +17,21 @@ const without = require("./without");
 const takeUntil = require("./takeUntil");
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  letterPositions: letterPositions,
-  map: map,
-  without: without,
-  takeUntil: takeUntil
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  without,
+  takeUntil
 };
