e7c640877c25798c30aedaa3c409245e5db1e14d
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-02-10 12:40:23 -0500

    export modules

--- head.js
+++ head.js
@@ -1,19 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`🔥🔥🔥Assertion failed:  ${actual}  !== ${expected}`);
-  } else {
-    console.log(`🍏🍏🍏Assertion passed: ${actual}  == ${expected}`);
-  }
-};
-
-
+const {assertEqual} = require("./assertEqual");
 const head = function(arr) {
   return arr[0];
 }
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hello"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head([5, "Hello", "Lighthouse", "Labs"]), "5");
-assertEqual(head([5, "Hello", "Lighthouse", "Labs"]), 5);
+
+module.exports = {head}
